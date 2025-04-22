f21c3a63f11ab239572d7cc06572ed356315859d
Lucy <solouser019@gmail.com>
2025-04-22 14:58:38 +0000

    Some Talks on Data types

---
+++ 01_basic/dataTypes.js
@@ -0,0 +1,6 @@
+// There are mainly two data types are there one is primitive and second one is Reference(non primitive)
+// In primitive
+// There are 7 Types => String | Number | BIGINT | Boolean | null | Undefined |Symbol 
+
+// In Reference (non primitive)
+// Object {} | Arrays[] | function ()
