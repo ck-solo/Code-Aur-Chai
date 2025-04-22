96b6404e4edc9850f78ad25647bac853479dcc96
Lucy <solouser019@gmail.com>
2025-04-22 11:27:08 +0000

    Datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,13 @@
+// "use strict"  // treat all code in newer code version
+// number => 2 to power 23
+// biginit
+// string => ""
+//boolean => true and false
+// null => standalone value
+// undefined => 
+//  symbol => unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
