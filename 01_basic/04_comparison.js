597970987b5cf3ef221a70032d6ee0f1865416bc
Lucy <solouser019@gmail.com>
2025-04-22 14:45:24 +0000

    comparison opertors

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,9 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+
+
+// (===) this triple equal is strong comaprison operator
+console.log(2 === 2); //true
+console.log("2" === 2); //false
