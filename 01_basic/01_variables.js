9078c058b59f73f03a232ba3ad12d777bb9f17bc
Lucy <solouser019@gmail.com>
2025-04-22 11:14:31 +0000

    video-1 completed

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 123;
+let accountEmail = "123@gmai.com"
+var accountPass = 12313
+accountCity = 'Delhi'
+
+// accountId = 2;
+accountEmail = "2@gmai.com"
+accountPass = 13
+accountCity = "Bengal"
+
+console.table([accountId, accountEmail, accountPass, accountCity])
