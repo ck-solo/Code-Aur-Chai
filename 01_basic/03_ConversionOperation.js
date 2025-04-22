f1738ed5dbffac5531b9d2d480f8fa65c0599c44
Lucy <solouser019@gmail.com>
2025-04-22 14:34:04 +0000

    some talks on conversion operations

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,30 @@
+// let score = 33;
+// console.log(typeof score);
+// console.log(typeof(score));
+/*
+"33" => 33
+"33abc" => NaN
+true => 1
+false => 0
+*/
+// -------------------------Operation-----------------------------------
+
+// console.log(2+2)  // 4
+// console.log(2-2)  // 0
+// console.log(2*2)  //4
+// console.log(2**2)  //8
+// console.log(2/2)  //1
+// console.log(2%2)  //0
+
+let num1 = 20;
+let num2 = 21
+let num3 = num1 + num2
+console.log(num3)  // 42
+
+console.log(1 + 2) //3
+console.log("1" + 2)  //12
+console.log(1 + "2") //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32
+
+
