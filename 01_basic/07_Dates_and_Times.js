938407b508b2abdf53d519d05158b2eb4866f6b6
Lucy <solouser019@gmail.com>
2025-04-23 04:46:30 +0000

    Some talks on Data and Times

---
+++ 01_basic/07_Dates_and_Times.js
@@ -0,0 +1,16 @@
+let createDate = new Date()
+// console.log(createDate)
+// console.log(createDate.toString())
+// console.log(createDate.toDateString())
+// console.log(createDate.toLocaleString()) 
+// console.log(typeof createDate)
+
+
+// let mydate = new Date(2023,0,23)
+let mydate = new Date("23-12-2024")
+// let mydate = new Date("2023-01-14 ")
+// console.log(mydate)
+
+let mytimestamp = Date.now()
+console.log(mytimestamp);
+
