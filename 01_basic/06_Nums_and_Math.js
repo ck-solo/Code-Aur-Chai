d328c53ecc21fdadfbd75c51b4af540c79972594
Lucy <solouser019@gmail.com>
2025-04-23 04:22:04 +0000

    Some talks about Nums and Math function

---
+++ 01_basic/06_Nums_and_Math.js
@@ -0,0 +1,22 @@
+const score = 300;
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+
+// const othernumber = 123.322
+// console.log(othernumber.toPrecision(4));
+
+// const hunderds = 100000000;
+// console.log(hunderds.toLocaleString('En-IN')) // ('En-IN') to change the number in indian numberical system.
+
+
+// -----------------------------------Math-------------------------------------
+
+console.log(Math.random()*10)
+console.log(Math.ceil(23.5))   //24
+console.log(Math.floor(23.6));  //23
+
+
