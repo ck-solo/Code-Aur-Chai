bfbc16712b15a00daa9818e4b948c4980968ba5e
Lucy <solouser019@gmail.com>
2025-10-15 20:30:44 +0530

    feat : add initial password generator component with state management and useCallback setup

--- Code-And-Chai-React/Day5_PasswordGenerator/src/App.jsx
+++ Code-And-Chai-React/Day5_PasswordGenerator/src/App.jsx
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 function App() {
-  const [character, setcharacter] = useState("Alphabet");
-  const [length, setlength] = useState(8);
-  const [password, setpassword] = useState("");
+  const [length, setLength] = useState(8);
+  const [character, setCharacter] = useState("Alphabet");
+  const [numbers, setNumbers] = useState('')
+  const [password, setPassword] = useState(""); 
+   
+  const passwordGenerator = useCallback(()=>{
+    let pass = ''
+    let string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if(numbers) string += '0123456789'
+    if(character) string += '!@#$%^&*()'
+    for(let i = 1; i <= array.length; i++){
+      let char = string[Math.floor(Math.random() * string.length)]
+      pass = string.charAt(char)
+    }
+    setPassword(pass)
+
+
+  }, [length,character,setPassword,numbers]);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-10">
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg  flex flex-col items-center justify-center h-screen gap-10">
       <h2 className="text-4xl font-bold">Password Generator</h2>
       <div>
         <form>
           <input className="border-white border-2 rounded-md   p-4" type="text" placeholder="Generate Password" />
           <br />
-          <button className="bg-zinc-400  p-2 mt-4 ">Generate</button>
+          <button onClick={()=>{passwordGenerator}} className="bg-zinc-400  p-2 mt-4 ">Generate</button>
           <br />
           <br />
           <div className="checkbox gap-10">
